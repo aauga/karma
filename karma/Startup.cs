0e23b8d66484756e38f1576a18068a61fc9304a2
Adomas Auga <hello@auga.dev>
2021-09-21 14:16:17 +0300

    Add data access singleton to Startup

--- karma/Startup.cs
+++ karma/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using karma.Data;
 
 namespace karma
 {
@@ -24,6 +25,7 @@ namespace karma
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<IDataAccess, DataAccess>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
