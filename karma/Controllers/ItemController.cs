bd0fc447e9ea32ca3388fdfb26f60122bfb6d680
Adomas Auga <hello@auga.dev>
2021-09-21 14:19:14 +0300

    Create item list GET request

--- karma/Controllers/ItemController.cs
+++ karma/Controllers/ItemController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using karma.Data;
+using karma.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace karma.Controllers
 {
@@ -6,6 +11,22 @@ namespace karma.Controllers
     [Route("/api/items")]
     public class ItemController : ControllerBase
     {
+        private readonly IDataAccess _data;
+        private readonly IConfiguration _config;
+
+        public ItemController(IDataAccess data, IConfiguration config)
+        {
+            _data = data;
+            _config = config;
+        }
         
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            string sql = "SELECT * FROM item_list";
+            List<ItemModel> items = await _data.LoadData<ItemModel, dynamic>(sql, new {}, _config.GetConnectionString("default"));
+
+            return Ok(items);
+        }
     }
 }
