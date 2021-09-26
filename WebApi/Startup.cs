adbea246c2a5fae60a1c38ae42f9d6ae4a8a576e
Adomas Auga <hello@auga.dev>
2021-09-26 12:54:50 +0300

    Update endpoint config

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -33,6 +34,16 @@ namespace WebApi
             app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}/{id?}");
+                
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("API is working");
+                });
+            });
         }
     }
 }
