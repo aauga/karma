ca90dbc208ef90920b41d0c70975558bf00f2fbf
Adomas Auga <hello@auga.dev>
2021-10-12 17:12:51 +0300

    Add new properties to Item

--- Domain/Entities/Item.cs
+++ Domain/Entities/Item.cs
@@ -4,16 +4,21 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 ﻿using System;
+ using Domain.Enums;
 
 
-namespace Domain.Entities
+ namespace Domain.Entities
 {
     public class Item
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public ItemCategories Categories { get; set; }
         [NotMapped]
         public List<IFormFile> PostedFiles { get; set; }
+        public string City { get; set; }
+        public string Uploader { get; set; }
+        public string Redeemer { get; set; }
     }
 }
