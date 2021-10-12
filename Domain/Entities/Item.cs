8bcf8f284c5dfd28e7ba4dc6401df9856bb45541
Adomas Auga <hello@auga.dev>
2021-10-12 17:21:35 +0300

    Update Item property naming

--- Domain/Entities/Item.cs
+++ Domain/Entities/Item.cs
@@ -1,10 +1,9 @@
 
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-﻿using System;
- using Domain.Enums;
+using Domain.Enums;
 
 
  namespace Domain.Entities
@@ -14,10 +13,10 @@ using System.ComponentModel.DataAnnotations.Schema;
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public ItemCategories Categories { get; set; }
+        public ItemCategories Category { get; set; }
+        public string City { get; set; }
         [NotMapped]
         public List<IFormFile> PostedFiles { get; set; }
-        public string City { get; set; }
         public string Uploader { get; set; }
         public string Redeemer { get; set; }
     }
