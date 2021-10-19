de8b8535f46e55c132a345cfecb7efcfff002fc7
Adomas Auga <hello@auga.dev>
2021-10-19 21:24:54 +0300

    Create string method extension

---
+++ Application/Core/Extensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Application.Core
+{
+    public static class Extensions
+    {
+        public static bool ContainsAll(this string value, string[] wordList)
+        {
+            foreach (string word in wordList)
+            {
+                if (!value.Contains(word))
+                {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+    }
+}
