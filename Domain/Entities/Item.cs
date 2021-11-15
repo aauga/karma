43ef8083bb3afde454d46e5fa4b516c5585c6cfd
Kristupas Gaidys <you@example.com>
2021-11-15 21:18:21 +0200

    Add user object

--- Domain/Entities/Item.cs
+++ Domain/Entities/Item.cs
@@ -6,22 +6,20 @@ using Domain.Enums;
 
 namespace Domain.Entities
 {
-    public class Item : IEquatable<Item>
+    public class Item
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public ItemCategories Category { get; set; }
+        public List<PointContributor> Contributions { get; set; }
         public string City { get; set; }
         [NotMapped]
         public List<IFormFile> PostedFiles { get; set; }
         [NotMapped]
         public List<String> ImageUrls { get; set; }
-        public string Uploader { get; set; }
-        public string Redeemer { get; set; }
-        public bool Equals(Item other)
-        {
-            return (Name, Description, Category, City) == (other.Name, other.Description, other.Category, other.City);
-        }
+        public User Uploader { get; set; }
+        public User Redeemer { get; set; }
+       
     }
 }
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public List<Item> Listings { get; set; }
+        public List<PointContributor> Contributions { get; set; }
+        public bool isVerified { get; set; }
+
+    }
+}
