5f349b3bf2e3dc69bd382391641bb6cf054b83a3
Kristupas Gaidys <you@example.com>
2021-11-15 22:36:30 +0200

    Add onRegister method

---
+++ Application/Users/OnRegister.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.User
+{
+    public class OnRegister
+    {
+        public class Command : IRequest
+        {
+            public string User { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly ItemDbContext _context;
+
+            public Handler(ItemDbContext context)
+            { 
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var check =_context.Users.FindAsync(request.User);
+                if (check == null)
+                {
+                    await _context.SaveChangesAsync();
+                }
+                return Unit.Value;
+            }
+        }
+    }
+}
