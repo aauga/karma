a8a7f3d45d97d04ab9b6445d381cebed68c3b68c
Adomas Auga <hello@auga.dev>
2021-11-16 01:49:55 +0200

    update file path in app tsx

--- WebUI/src/App.tsx
+++ WebUI/src/App.tsx
@@ -1,22 +1,15 @@
-
 import './App.css';
 import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from '@auth0/auth0-react';
 import Loading from './components/loading';
 import ProtectedRoute from './auth/protected-route';
-import AddListing from './pages/AddListing';
+import AddListing from './pages/AddListing.tsx';
 import ItemDetails from './components/ItemDetails';
 
 function App() {
@@ -27,14 +20,14 @@ function App() {
   }
 
   return (
-    <div id="app" className="d-flex flex-column h-100">
+    <div id='app' className='d-flex flex-column h-100'>
       <Header />
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/details/:id" exact component={ItemDetails} />
-      <ProtectedRoute path="/addlisting" component={AddListing} />
-      <ProtectedRoute path="/profile" component={Profile} />
-    </Switch>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/details/:id' exact component={ItemDetails} />
+        <ProtectedRoute path='/addlisting' component={AddListing} />
+        <ProtectedRoute path='/profile' component={Profile} />
+      </Switch>
     </div>
   );
 }
