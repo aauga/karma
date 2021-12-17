608ea2b10cf9f3e48104b9dd63b219d22ecbd7e0
Adomas Auga <hello@auga.dev>
2021-12-17 15:09:38 +0200

    Add ability to load certain amount of placeholders

--- WebUI/src/components/listings/ListingPlaceholders.tsx
+++ WebUI/src/components/listings/ListingPlaceholders.tsx
@@ -2,14 +2,18 @@ import { Col } from 'react-bootstrap';
 import styled from 'styled-components';
 import CardSkeleton from '../card/CardSkeleton';
 
+interface PlaceholderProps {
+    amount: number;
+}
+
 const StyledCol = styled(Col)`
     margin-bottom: 16px;
 `;
 
-const ListingPlaceholders: Function = () => {
+const ListingPlaceholders: Function = ({ amount }: PlaceholderProps) => {
     let list = [];
 
-    for (let i = 1; i <= 8; i++) {
+    for (let i = 1; i <= amount; i++) {
         list.push(
             <StyledCol md={6} lg={4} xl={3}>
                 <CardSkeleton key={i} />
