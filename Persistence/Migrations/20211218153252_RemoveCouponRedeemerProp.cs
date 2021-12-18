e2c39482e4083b9725f1d3810dee558596888c09
Adomas Auga <hello@auga.dev>
2021-12-18 18:59:10 +0200

    Create migration

---
+++ Persistence/Migrations/20211218153252_RemoveCouponRedeemerProp.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class RemoveCouponRedeemerProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Redeemer",
+                table: "CouponCodes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Redeemer",
+                table: "CouponCodes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
