223da2b9fdc4d06fbf71cf1773af0b6252f3fb7f
Adomas Auga <hello@auga.dev>
2021-12-18 18:59:45 +0200

    Rename exception

---
+++ Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Application.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException()
+            : base()
+        {
+        }
+
+        public BadRequestException(string message)
+            : base(message)
+        {
+        }
+    }
+}
