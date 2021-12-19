5025607cd7a1dbd695a283d1f43c63408e78e5f6
Adomas Auga <hello@auga.dev>
2021-12-19 12:05:37 +0200

    Create active listing and applicant queries

---
+++ Application/Listings/Queries/GetActiveListingsQuery/GetActiveListingsQuery.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Exceptions;
+using Domain.Entities;
+using MediatR;
+using Persistence;
+
+namespace Application.Listings.Queries.GetActiveListingsQuery
+{
+    public class GetActiveListingsQuery : IRequest<IEnumerable<Item>>
+    {
+        public string UserId { get; set; }
+    }
+
+    public class Handler : IRequestHandler<GetActiveListingsQuery, IEnumerable<Item>>
+    {
+        private readonly ItemDbContext _context;
+
+        public Handler(ItemDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IEnumerable<Item>> Handle(GetActiveListingsQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.FindAsync(request.UserId);
+
+            if (user == null)
+            {
+                throw new NotFoundException(nameof(User), request.UserId);
+            }
+            
+            return _context.Items.Where(item => !item.IsSuspended && item.Uploader == user.Username);
+        }
+    }
+}
---
+++ Application/Listings/Queries/GetApplicationsQuery/GetApplicationsQuery.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Models;
+using Application.Exceptions;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Listings.Queries.GetApplicationsQuery
+{
+    public class GetApplicationsQuery : IRequest<IEnumerable<ApplicationModel>>
+    {
+        public Guid ItemId { get; set; }
+        public string UserId { get; set; }
+    }
+
+    public class Handler : IRequestHandler<GetApplicationsQuery, IEnumerable<ApplicationModel>>
+    {
+        private readonly ItemDbContext _context;
+
+        public Handler(ItemDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IEnumerable<ApplicationModel>> Handle(GetApplicationsQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users.FindAsync(request.UserId);
+
+            if (user == null)
+            {
+                throw new NotFoundException(nameof(User), request.UserId);
+            }
+
+            var item = await _context.Items
+                .Where(item => item.Id == request.ItemId)
+                .Include(item => item.Applicants)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (item == null)
+            {
+                throw new NotFoundException(nameof(Item), request.ItemId);
+            }
+
+            var list = new List<ApplicationModel>();
+
+            foreach (var applicant in item.Applicants)
+            {
+                var applicantData = _context.Users.First(x => x.AuthId == applicant.UserId);
+                list.Add(new ApplicationModel {Username = applicantData.Username, Reason = applicant.Reason, IsSuspended = false});
+            }
+
+            return list;
+        }
+    }
+}
