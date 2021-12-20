7fe9c9849ea9f8582c717eb2b6d28e321df88349
Adomas Auga <hello@auga.dev>
2021-12-20 02:28:00 +0200

    Fix item control page layout

--- WebUI/src/pages/Listings.tsx
+++ WebUI/src/pages/Listings.tsx
@@ -7,14 +7,9 @@ const Listings = () => {
             <h2 className='my-3'>My Active Listings</h2>
 
             <Tab.Container>
-                <Row>
-                    <Col sm={4}>
-                        <Nav variant='pills' className='flex-column'>
-                            <ActiveListings />
-                        </Nav>
-                    </Col>
-                    <Col sm={8}></Col>
-                </Row>
+                <Nav variant='pills' className='flex-column'>
+                    <ActiveListings />
+                </Nav>
             </Tab.Container>
         </Container>
     );
